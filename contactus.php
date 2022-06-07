8c838a615e623fb1a20d7c68c05207426bda0932
ManjuCusat <102962156+ManjuCusat@users.noreply.github.com>
2022-06-07 23:20:28 +0530

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en" style="scroll-behavior:smooth">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CUSAT</title>
+    <link rel="stylesheet" href="http://cs.cusat.ac.in/images/index_04.jpg" type="jpeg/gif" sizes="16x16">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"/>
+</head>
+<body>
+<header class="bg-dark" style="position:fixed; width: 100%; z-index: 1">
+    <div class="row text-white">
+        <div class="col-md-1 col-2 p-4 pl-5">
+          <img Src="red_logo.png" type="jpeg/gif/png" width="70" height="80">
+        </div>
+        <div class="col-md-5 col-8 p-4 my-auto text-white">
+          <h2>COCHIN UNIVERSITY OF SCIENCE AND TECHNOLOGY</h2>
+        </div>
+        <div class="col-md-6 col-12 my-auto mr-auto">
+          <div class="row">
+              <div>
+                  <form class="d-flex" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success text-white" type="submit">Go</button>
+              </form>
+             </div>
+          </div>
+        </div>
+     </div>
+</header>
+<section class="bg-white" style="padding-top: 10em">
+  <div class="container-fluid">
+    <div class="row align-items-center">
+            <div class="container-fluid">
+                    <div class="row flex-nowrap">
+                        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-white">
+                            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                                    <li class="nav-item">
+                                        <a href="index.html" class="nav-link align-middle px-0">
+                                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#submenu1 content_page.html" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                                            <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">About</span> </a>
+                                        <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                                            <li class="w-100">
+                                                <a href="aboutc.html" class="nav-link px-0"> <span class="d-none d-sm-inline">CUSAT</span> </a>
+                                            </li>
+                                            <li>
+                                                <a href="aboutd.html" class="nav-link px-0"> <span class="d-none d-sm-inline">DCS</span> </a>
+                                            </li>
+						    </ul>
+                                    </li>
+						<li>
+                                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                                            <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Academic</span> </a>
+                                        <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                                            <li class="w-100">
+                                                <a href="programmes.php" class="nav-link px-0"> <span class="d-none d-sm-inline">Programmes</span>  </a>
+                                            </li>
+                                            <li>
+                                                <a href="people.php" class="nav-link px-0"> <span class="d-none d-sm-inline">People</span> </a>
+                                            </li>
+                                            <li>
+                                                <a href="research.php" class="nav-link px-0"> <span class="d-none d-sm-inline">Research</span> </a>
+                                            </li>
+                                        </ul>
+                                    </li>
+
+                                    <li>
+                                    <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                                      <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Admission</span> </a>
+                                    <ul class="collapse flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                                      <li class="w-100">
+                                        <a href="eligibility.php" class="nav-link px-0"> <span class="d-none d-sm-inline">Eligibility</span>  </a>
+                                      </li>
+                                      <li>
+                                        <a href="feestructure.php" class="nav-link px-0"> <span class="d-none d-sm-inline">Fee Structure</span> </a>
+                                      </li>
+                                      <li>
+                                        <a href="phd.php" class="nav-link px-0"> <span class="d-none d-sm-inline">Ph.D</span> </a>
+                                      </li>
+                                    </ul>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a href="index.html" class="nav-link align-middle px-0">
+                                      <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Contact us</span>
+                                    </a>
+                                </ul>
+                                <hr>
+                               
+                            </div>
+                        </div>
+                        <div class="col py-3" id="cusat">
+                                <h3>Contact us</h3>
+                                <p>
+                                Cochin University of Science & Technology (CUSAT)<br>
+                                <br>
+                                Cochin 682022, Kerala, India<br>
+                                <br>
+                                Email:csdir@cusat.ac.in<br>
+                                <br>
+                                Phone:+91-484-2862301<br>
+                                <br>
+                                Fax:+91-484-2576368<br>
+                                <br>       
+                                </p>.
+                        </div>
+                    </div>
+                </div>
+
+    </div>
+</div>
+<footer class="bg-dark" id="Footer">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12 text-center text-white">
+        <p> Copyrights © 2021 Department of Computer Science, CUSAT. All rights reserved | Disclaimer | Contact<br>
+        </p>
+      </div>
+    </div>
+  </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/15b6a24c50.js" crossorigin="anonymous"></script>
+</body>
+</html>
